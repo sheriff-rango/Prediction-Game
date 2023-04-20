650a5d7f6aa0f38755c296c3a95c48fb29032c34
Master Shifu <sheriff0613@gmail.com>
2023-04-21 03:51:25 +0800

    [WIP]: added execute transaction logic for SEI chain

--- src/Updater.ts
+++ src/Updater.ts
@@ -7,54 +7,58 @@ import { balanceState } from "state/userInfo"
 import useContract from "hooks/useContract"
 
 export default function Updater(): null {
-  const [, setBtcPrice] = useRecoilState(btcPriceState)
-  const [, setBalance] = useRecoilState(balanceState)
-  const { getBalance } = useContract()
+    const [, setBtcPrice] = useRecoilState(btcPriceState)
+    const [, setBalance] = useRecoilState(balanceState)
+    const { getBalance } = useContract()
 
-  const { data: btcPriceResponse } = useQuery({
-    queryKey: ["btcPrice"],
-    queryFn: () =>
-      axios
-        .get("https://api.coingecko.com/api/v3/coins/bitcoin")
-        .then((res) => res.data),
-    refetchInterval: 1000 * 10
-  })
+    const { data: btcPriceResponse } = useQuery({
+        queryKey: ["btcPrice"],
+        queryFn: () =>
+            axios
+                .get("https://api.coingecko.com/api/v3/coins/bitcoin")
+                .then((res) => res.data),
+        refetchInterval: 1000 * 10
+    })
 
-  const { data: balanceResopnse } = useQuery({
-    queryKey: ["balance"],
-    queryFn: () => getBalance(),
-    refetchInterval: 1000
-  })
+    const { data: balanceResopnse } = useQuery({
+        queryKey: ["balance"],
+        queryFn: () => getBalance(),
+        refetchInterval: 1000
+    })
 
-  useEffect(() => {
-    if (btcPriceResponse?.market_data) {
-      const priceNumber = btcPriceResponse.market_data.current_price?.usd || 0
-      const price = priceNumber.toLocaleString("en-US", {
-        maximumFractionDigits: 3
-      })
-      const priceChangeNumber =
-        Math.round(
-          (btcPriceResponse.market_data.price_change_percentage_1h_in_currency
-            ?.usd || 0) * 1000
-        ) / 1000
-      const priceChange =
-        priceChangeNumber === 0
-          ? ""
-          : priceChangeNumber > 0
-          ? `+${priceChangeNumber}%`
-          : `${priceChangeNumber}%`
-      const state = priceChangeNumber > 0 ? 1 : priceChangeNumber < 0 ? -1 : 0
-      setBtcPrice({
-        price,
-        priceChange,
-        state
-      })
-    }
-  }, [btcPriceResponse])
+    useEffect(() => {
+        if (btcPriceResponse?.market_data) {
+            const priceNumber =
+                btcPriceResponse.market_data.current_price?.usd || 0
+            const price = priceNumber.toLocaleString("en-US", {
+                maximumFractionDigits: 3
+            })
+            const priceChangeNumber =
+                Math.round(
+                    (btcPriceResponse.market_data
+                        .price_change_percentage_1h_in_currency?.usd || 0) *
+                        1000
+                ) / 1000
+            const priceChange =
+                priceChangeNumber === 0
+                    ? ""
+                    : priceChangeNumber > 0
+                    ? `+${priceChangeNumber}%`
+                    : `${priceChangeNumber}%`
+            const state =
+                priceChangeNumber > 0 ? 1 : priceChangeNumber < 0 ? -1 : 0
+            setBtcPrice({
+                price,
+                priceChange,
+                state
+            })
+        }
+    }, [btcPriceResponse])
 
-  useEffect(() => {
-    if (balanceResopnse && !isNaN(balanceResopnse)) setBalance(balanceResopnse)
-  }, [balanceResopnse])
+    useEffect(() => {
+        if (balanceResopnse && !isNaN(balanceResopnse))
+            setBalance(balanceResopnse)
+    }, [balanceResopnse])
 
-  return null
+    return null
 }
--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -1,63 +1,112 @@
 import {
-  getCosmWasmClient,
-  getSigningCosmWasmClient,
-  getQueryClient
+    getCosmWasmClient,
+    getSigningCosmWasmClient,
+    getQueryClient
 } from "@sei-js/core"
 import { useCallback } from "react"
 import { useChain } from "@cosmos-kit/react"
 import { ExtendedHttpEndpoint } from "@cosmos-kit/core"
 import { useQueryClient } from "@sei-js/react"
 import { cosmos } from "juno-network"
+import { coins } from "@cosmjs/proto-signing"
 import { AssetInfo, ChainInfo } from "../constants"
+import { toMicroAmount } from "utils/tokens/coins"
+import { StdFee } from "@cosmjs/stargate"
 
 const useContract = () => {
-  const { getRpcEndpoint, address } = useChain("seimainnet")
+    const { getRpcEndpoint, address, getSigningCosmWasmClient, estimateFee } =
+        useChain("seimainnet")
 
-  const getRpcEndpointString = useCallback(async () => {
-    let rpcEndpoint = await getRpcEndpoint()
+    const getRpcEndpointString = useCallback(async () => {
+        let rpcEndpoint = await getRpcEndpoint()
 
-    if (!rpcEndpoint) {
-      console.info("no rpc endpoint — using a fallback")
-      rpcEndpoint = ChainInfo.apis.rpc[0].address
+        if (!rpcEndpoint) {
+            console.info("no rpc endpoint — using a fallback")
+            rpcEndpoint = ChainInfo.apis.rpc[0].address
+        }
+        return typeof rpcEndpoint === "string"
+            ? rpcEndpoint
+            : (rpcEndpoint as ExtendedHttpEndpoint).url
+    }, [getRpcEndpoint])
+
+    const runQuery = useCallback(
+        async (contractAddress: string, queryMsg: any) => {
+            const rpcEndpoint = await getRpcEndpointString()
+            const cosmwasmClient = await getCosmWasmClient(rpcEndpoint)
+            const result = await cosmwasmClient.queryContractSmart(
+                contractAddress,
+                queryMsg
+            )
+            return result
+        },
+        [getRpcEndpointString]
+    )
+
+    const getBalance = useCallback(async () => {
+        if (!address) return 0
+        // get RPC client
+        const rpcEndpoint = await getRpcEndpointString()
+        const client = await cosmos.ClientFactory.createRPCQueryClient({
+            rpcEndpoint
+        })
+        // fetch balance
+        const balance = await client.cosmos.bank.v1beta1.balance({
+            address,
+            denom: AssetInfo.assets[0].denom_units[0].denom
+        })
+
+        return Number(balance?.balance?.amount || 0) / 1e6
+    }, [address, getRpcEndpointString])
+
+    const runExecute = useCallback(
+        async (
+            contractAddress: string,
+            executeMsg: any,
+            option?: {
+                memo?: string
+                funds?: string
+                denom?: string
+            }
+        ) => {
+            if (!address) return
+            const executeMemo = option?.memo || ""
+            const executeFunds = option?.funds || ""
+            const executeDenom =
+                option?.denom || AssetInfo.assets[0].denom_units[0].denom
+
+            const fee: StdFee = {
+                amount: [
+                    {
+                        denom: executeDenom,
+                        amount: "1"
+                    }
+                ],
+                gas: "86364"
+            }
+
+            const signingCosmWasmClient = await getSigningCosmWasmClient()
+            return signingCosmWasmClient.execute(
+                address,
+                contractAddress,
+                executeMsg,
+                fee,
+                executeMemo,
+                executeFunds
+                    ? coins(
+                          toMicroAmount(executeFunds, executeDenom),
+                          executeDenom
+                      )
+                    : undefined
+            )
+        },
+        [getSigningCosmWasmClient, address]
+    )
+
+    return {
+        getBalance,
+        runQuery,
+        runExecute
     }
-    return typeof rpcEndpoint === "string"
-      ? rpcEndpoint
-      : (rpcEndpoint as ExtendedHttpEndpoint).url
-  }, [getRpcEndpoint])
-
-  const runQuery = useCallback(
-    async (contractAddress: string, queryMsg: any) => {
-      const rpcEndpoint = await getRpcEndpointString()
-      const cosmwasmClient = await getCosmWasmClient(rpcEndpoint)
-      const result = await cosmwasmClient.queryContractSmart(
-        contractAddress,
-        queryMsg
-      )
-      return result
-    },
-    [getRpcEndpointString]
-  )
-
-  const getBalance = useCallback(async () => {
-    if (!address) return 0
-    // get RPC client
-    const rpcEndpoint = await getRpcEndpointString()
-    const client = await cosmos.ClientFactory.createRPCQueryClient({
-      rpcEndpoint
-    })
-    // fetch balance
-    const balance = await client.cosmos.bank.v1beta1.balance({
-      address,
-      denom: AssetInfo.assets[0].denom_units[0].denom
-    })
-
-    return Number(balance?.balance?.amount || 0) / 1e6
-  }, [address, getRpcEndpointString])
-
-  return {
-    getBalance,
-    runQuery
-  }
 }
 
 export default useContract
